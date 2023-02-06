c4ad5a9f0d9613d75987916a0af117d6051e97af
samarth yadav <121474767+Sammarth-web@users.noreply.github.com>
2023-02-06 15:50:16 +0530

    Uploaded final project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto : ["Roboto"],
+        poppins : ["Poppins"]
+        
+      },
+
+      
+
+      colors: {
+
+        lightblue : "#D8EFFF",
+        blue : "#3937F7",
+        yellow : "#FFC932"
+         
+        
+      },
+    },
+  },
+  plugins: [],
+}
